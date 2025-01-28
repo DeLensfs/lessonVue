9b09f46344f232f92e296dd24031f3ad408fd982
De Lens <lensik46@gmail.com>
2025-01-28 09:42:12 +0300

    variadles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,47 @@
-<script>
-
-</script>
-
 <template>
- 
+  <h3>{{ info }}</h3>
+  <p>{{ someInfo }}</p>
+
+  <button type="button" @click="userData('Some title')">Отправить</button>
 </template>
 
-<style scoped>
 
+<script>
+export default {
+  data() {
+    return {
+      info: 'Title!',
+      someInfo: 'Anons of message'
+    }
+  },
+  methods: {
+    userData(word) {
+      this.info=word;
+      // this.info = 'Some Title'
+      this.someInfo = 'Some Paragraph'
+
+    }
+  }
+}
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+<style scoped>
+h3 {
+  font-weight: lighter;
+}
+
+p {
+  color: rgb(160, 28, 28);
+}
 </style>
