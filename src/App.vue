f16d7900e3592acb50e67e3d0de655c3f4482d33
De Lens <lensik46@gmail.com>
2025-01-28 12:43:55 +0300

    словия и циклы

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
-  <h3>{{ info }}</h3>
-  <p>{{ someInfo }}</p>
+  <input type="text" v-model="userName" placeholder="Имя">
+  <input type="password" v-model="userPass" placeholder="Пароль">
+  <input type="email" v-model="userEmail" placeholder="Email">
+  <p className="error">{{ error }}</p>
+  <button @click="sendData()">Отправить</button>
 
-  <button type="button" @click="userData('Some title')">Отправить</button>
+<div v-if="users.length==0" className="user">
+  <p>У нас нет пользователей</p>
+</div>
+<div v-else-if="users.length==1" className="user">
+  <p>У нас только один пользователь</p>
+</div>
+<div v-else className="user">
+  <p>У нас больше одного пользователя</p>
+</div>
+
+<div v-for="(el, index) in users" :key="index" className="user">
+  <h3>{{ el.name }}</h3>
+<p>{{ el.email }} - <b>{{ el.pass }}</b></p>
+</div>
+
+  <!-- <p>{{ users }}</p> -->
 </template>
 
 
@@ -10,16 +28,34 @@
 export default {
   data() {
     return {
-      info: 'Title!',
-      someInfo: 'Anons of message'
+      users: [],
+      error: '',
+      userName: '',
+      userPass: '',
+      userEmail: '',
+
+
     }
   },
   methods: {
-    userData(word) {
-      this.info=word;
-      // this.info = 'Some Title'
-      this.someInfo = 'Some Paragraph'
+    sendData() {
+      if (this.userName == '') {
+        this.error = 'Имя не введено';
+        return;
+      } else if (this.userPass == '') {
+        this.error = 'Пароль не введено';
+        return;
+      } else if (this.userEmail == '') {
+        this.error = 'Email не введено';
+        return;
+      }
+      this.error='';
+      this.users.push({
+        name: this.userName,
+        pass: this.userPass,
+        email: this.userEmail,
 
+      })
     }
   }
 }
@@ -31,17 +67,13 @@ export default {
 
 
 
-
-
-
-
-
 <style scoped>
-h3 {
-  font-weight: lighter;
+.user{
+  background-color: grey;
+  margin: 1rem;
+  color: white;
 }
-
-p {
-  color: rgb(160, 28, 28);
+.user p, .user h3{
+  padding: 5px;
 }
 </style>
